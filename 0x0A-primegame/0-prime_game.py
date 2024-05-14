0752186a0d1b6779c6396248c464186761ae1f69
root <RobinLeeHenry101Gmail.com>
2024-05-14 07:25:51 -0700

    fixed the problem

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,48 +2,36 @@
 """0-prime_game.py"""
 
 
+def generate_primes(limit):
+    """Generates primes"""
+    primes = [True] * (limit + 1)
+    primes[0], primes[1] = False, False
+    for i in range(2, int(limit ** 0.5) + 1):
+        if primes[i]:
+            for j in range(i * i, limit + 1, i):
+                primes[j] = False
+    return primes
+
+
+def count_primes(primes, n):
+    """Counts the number of primes"""
+    return sum(1 for prime in primes[:n] if prime)
+
+
 def isWinner(x, nums):
-    # Function to check if a number is prime
-    def is_prime(num):
-        if num <= 1:
-            return False
-        if num <= 3:
-            return True
-        if num % 2 == 0 or num % 3 == 0:
-            return False
-        i = 5
-        while i * i <= num:
-            if num % i == 0 or num % (i + 2) == 0:
-                return False
-            i += 6
-        return True
-
-    def find_primes(n):
-        primes = []
-        for i in range(2, n + 1):
-            if is_prime(i):
-                primes.append(i)
-        return primes
-
-    def play_game(n):
-        primes = find_primes(n)
-        return "Ben" if len(primes) % 2 == 0 else "Maria"
-
-    maria_wins = 0
-    ben_wins = 0
-    for n in nums:
-        winner = play_game(n)
-        if winner == "Maria":
-            maria_wins += 1
-        elif winner == "Ben":
-            ben_wins += 1
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+    """Determines the winner of a prime game session"""
+    if x < 1 or not nums:
         return None
 
-print("Winner: {}".format(isWinner(3, [4, 5, 1])))
-print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
+    marias_wins, bens_wins = 0, 0
+    max_num = max(nums)
+    primes = generate_primes(max_num)
+
+    for n in nums:
+        primes_count = count_primes(primes, n)
+        bens_wins += primes_count % 2 == 0
+        marias_wins += primes_count % 2 == 1
+
+    if marias_wins == bens_wins:
+        return None
+    return 'Maria' if marias_wins > bens_wins else 'Ben'
